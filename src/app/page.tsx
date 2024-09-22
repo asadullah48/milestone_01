8642ce122f84fc28eccbcd38b3b72fb87b25b2d3
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-09-22 11:37:57 +0500

    edited for improment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
- export default function Home() {
-  return ( 
-    
-      <h1 className="text-9xl grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">Hello World</h1>
-         
-  );
-}       
       
+    import {Protest_Guerrilla} from "next/font/google"
+    const font = Protest_Guerrilla({weight:"400" , subsets:['latin-ext']})
+    
+    export default function Home() {
+      return (
+        <div className={`h-50 w-1/2  shadow-xl shadow-blue-600 flex justify-center items-center  rounded-5xl m-auto mt-52`}>
+          <h1 className={`text-white ${font.className} text-9xl hover:text-blue-500 transition  font-sans animate-pulse`}>Hello World</h1>
+        </div>
+      );
+    }
